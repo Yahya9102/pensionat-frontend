c90721bf72c5617d0b865425d77864490f9b9235
Yahya9102 <yaya_hussein@hotmail.com>
2026-05-21 13:01:01 +0200

    add booking and show all rooms

--- app.js
+++ app.js
@@ -22,3 +22,44 @@ async function createCustomer() {
 function showMessage(text) {
     document.getElementById("message").textContent = text;
 }
+
+
+async function loadRooms() {
+    const response = await fetch(`${API_URL}/rooms`);
+
+    const rooms = await response.json();
+
+    const list = document.getElementById("rooms");
+
+    list.innerHTML = ""
+
+    rooms.forEach(room => {
+        const li = document.createElement("li")
+        li.textContent = `Rum ${room.roomNumber}, ${room.beds} sängar, ${room.pricePerNight} kr/natt`
+        list.append(li)
+    })
+}
+
+
+
+async function createBooking() {
+    const booking = {
+        customerId: document.getElementById("customerId").value,
+        roomId: document.getElementById("roomId").value,
+        startDate: document.getElementById("startDate").value,
+        endDate: document.getElementById("endDate").value
+    };
+
+      const response = await fetch(`${API_URL}/bookings`, {
+        method: "POST",
+        headers: {"Content-type": "application/json"},
+        body: JSON.stringify(booking)
+    });
+
+    showMessage(response.ok ? "Bokning skapad" : "Bokningen kunde inte skapas")
+
+}
+
+
+
+
